8afe5e52068d144278c92c99e5e2cc849c861244
gio797 <119562363+gio797@users.noreply.github.com>
2023-07-07 13:57:11 +0400

    add thans img

--- src/pages/Page5.jsx
+++ src/pages/Page5.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import sidebar from "../assets/images/bg-sidebar-mobile.svg";
+import img from "../assets/images/icon-thank-you.svg";
 
 function Page5({ planType, formData }) {
   return (
     <main>
       <img src={sidebar} alt="" className="side-bar-mob" />
       <form className="form">
+        <img src={img} alt="icon" className="thank-you-icon" />
         <h1 className="title">Thank you</h1>
         <p className="text">
           Thanks for confirming your subscription! <br />
