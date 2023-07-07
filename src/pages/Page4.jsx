61a033a0f6b59ef52760e6647dc1b786f709b444
gio797 <119562363+gio797@users.noreply.github.com>
2023-07-07 16:46:27 +0400

    add total numbers

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import sidebar from "../assets/images/bg-sidebar-mobile.svg";
 
 function Page4({ planType, formData }) {
   const navigate = useNavigate();
+
+  const [planPrice, setPlanPrice] = useState(null);
+
+  const [monthlyOnlineService, setMonthlyOnlineService] = useState(null);
+  const [monthlyLargeStorage, setMonthlyLargeStorage] = useState(null);
+  const [monthlyCustomizableProfile, setMonthlyCustomizableProfile] =
+    useState(null);
+
+  const [yearlyOnlineService, setYearlyOnlineService] = useState(null);
+  const [yearlyLargeStorage, setYearlyLargeStorage] = useState(null);
+  const [yearlyCustomizableProfile, setYearlyCustomizableProfile] =
+    useState(null);
+
+  const total =
+    planPrice +
+    monthlyOnlineService +
+    monthlyLargeStorage +
+    monthlyCustomizableProfile +
+    yearlyOnlineService +
+    yearlyLargeStorage +
+    yearlyCustomizableProfile;
+
+  useEffect(() => {
+    if (formData.plan === "monthly-arcade") {
+      setPlanPrice(9);
+    } else if (formData.plan === "monthly-advanced") {
+      setPlanPrice(12);
+    } else if (formData.plan === "monthly-pro") {
+      setPlanPrice(15);
+    } else if (formData.plan === "yearly-arcade") {
+      setPlanPrice(90);
+    } else if (formData.plan === "yearly-advanced") {
+      setPlanPrice(120);
+    } else if (formData.plan === "yearly-pro") {
+      setPlanPrice(150);
+    }
+  }, [formData.plan]);
+
+  useEffect(() => {
+    if (formData.monthlyCustomizableProfile) {
+      setMonthlyCustomizableProfile(2);
+    }
+  }, [formData.monthlyCustomizableProfile]);
+
+  useEffect(() => {
+    if (formData.monthlyOnlineService) {
+      setMonthlyOnlineService(1);
+    }
+  }, [formData.monthlyOnlineService]);
+
+  useEffect(() => {
+    if (formData.monthlyLargeStorage) {
+      setMonthlyLargeStorage(2);
+    }
+  }, [formData.monthlyLargeStorage]);
+
+  useEffect(() => {
+    if (formData.yearlyCustomizableProfile) {
+      setYearlyCustomizableProfile(20);
+    }
+  }, [formData.yearlyCustomizableProfile]);
+
+  useEffect(() => {
+    if (formData.yearlyOnlineService) {
+      setYearlyOnlineService(10);
+    }
+  }, [formData.yearlyOnlineService]);
+
+  useEffect(() => {
+    if (formData.yearlyLargeStorage) {
+      setYearlyLargeStorage(20);
+    }
+  }, [formData.yearlyLargeStorage]);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -21,6 +94,65 @@ function Page4({ planType, formData }) {
         </p>
         <div>
           <h2>{formData.plan}</h2>
+          {planType ? <p>${planPrice}/mo</p> : <p>${planPrice}/yr</p>}
+          <hr />
+
+          {monthlyOnlineService ? (
+            <div>
+              <p>Online service</p>
+              <p>${monthlyOnlineService}/mo</p>
+            </div>
+          ) : null}
+
+          {monthlyLargeStorage ? (
+            <div>
+              <p>Large Storage</p>
+              <p>${monthlyLargeStorage}/mo</p>
+            </div>
+          ) : null}
+
+          {monthlyCustomizableProfile ? (
+            <div>
+              <p>Customizable profile</p>
+              <p>${monthlyCustomizableProfile}/mo</p>
+            </div>
+          ) : null}
+
+          {yearlyOnlineService ? (
+            <div>
+              <p>Online service</p>
+              <p>${yearlyOnlineService}/yr</p>
+            </div>
+          ) : null}
+
+          {yearlyLargeStorage ? (
+            <div>
+              <p>Large Storage</p>
+              <p>${yearlyLargeStorage}/yr</p>
+            </div>
+          ) : null}
+
+          {yearlyCustomizableProfile ? (
+            <div>
+              <p>Customizable profile</p>
+              <p>${yearlyCustomizableProfile}/yr</p>
+            </div>
+          ) : null}
+
+          <br />
+          <br />
+
+          {planType ? (
+            <div>
+              <p>Total(per month)</p>
+              <p>${total}/mo</p>
+            </div>
+          ) : (
+            <div>
+              <p>Total(per year)</p>
+              <p>${total}/yr</p>
+            </div>
+          )}
         </div>
         <button className="next-page-btn">confirm</button>
       </form>
